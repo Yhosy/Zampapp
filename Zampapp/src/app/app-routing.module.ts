c3637f8c0d484fe8146c1f4a07a87d714d792b3e
Angel <yhosy_soldier@hotmail.com>
2018-02-19 12:14:05 +0100

    Componente home

---
+++ Zampapp/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+ 
+
+import {AppComponent} from './app.component'
+import {HomeComponent} from './home/home.component'
+
+
+ 
+const routes: Routes = [
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+];
+ 
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}

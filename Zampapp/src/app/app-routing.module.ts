41223f354092afd8145bbe0397be9c555fcfacf3
marcosalbares <marcosalbares@gmail.com>
2018-02-19 18:13:16 +0100

    a

--- Zampapp/src/app/app-routing.module.ts
+++ Zampapp/src/app/app-routing.module.ts
@@ -16,17 +16,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
   { path: 'Resultados', component: ResultadosComponent },
-<<<<<<< HEAD
   { path: 'Preguntas', component: PreguntasComponent },
-=======
-  { path: 'Preguntas', component: HomeComponent },
-<<<<<<< HEAD
   { path: 'pagplato', component: PlatoDetailComponent }
->>>>>>> a5ecc2380d961b0aeb6523fb093a45c1e3e83e70
-
-  
-=======
->>>>>>> 75e1ea978e7dc3d702426e7f58117b7aa306384c
 ];
  
 @NgModule({

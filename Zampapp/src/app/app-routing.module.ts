56af4d15ee145a5496d274a08c18a700200e03ec
marcosalbares <marcosalbares@gmail.com>
2018-02-19 18:09:00 +0100

    ultima hora

--- Zampapp/src/app/app-routing.module.ts
+++ Zampapp/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {AppComponent} from './app.component'
 import {HomeComponent} from './home/home.component'
 import {ResultadosComponent} from './resultados/resultados.component'
-// import {PreguntasComponent} from './preguntas/preguntas.component'
+import {PreguntasComponent} from './preguntas/preguntas.component'
 
 
 
@@ -14,7 +14,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
   { path: 'Resultados', component: ResultadosComponent },
-  { path: 'Preguntas', component: HomeComponent },
+  { path: 'Preguntas', component: PreguntasComponent },
 
   
 ];
--- Zampapp/src/app/app.module.ts
+++ Zampapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
@@ -8,19 +9,22 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ResultadosComponent } from './resultados/resultados.component';
+import { PreguntasComponent } from './preguntas/preguntas.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
     HomeComponent,
-    ResultadosComponent
+    ResultadosComponent,
+    PreguntasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

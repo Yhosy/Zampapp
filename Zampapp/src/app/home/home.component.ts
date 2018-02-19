0f8d4989ed521bf42136adfadd8cb130d1b363f2
Angel <yhosy_soldier@hotmail.com>
2018-02-19 17:31:04 +0100

    Home Actualizado

--- Zampapp/src/app/home/home.component.ts
+++ Zampapp/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Categorias } from '../categorias';
+import {BusquedaService} from '../busqueda.service'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  filtrado = [];
+  valores = {};
   constructor() { }
 
   ngOnInit() {
+    this.Escribir('');
+    console.log(BusquedaService.CogerFiltrador)
+    
   }
 
-}
+  Escribir(texto) {
+    this.filtrado = Categorias.filter((element, i) => {
+      return element.value.indexOf(texto) >= 0;
+    })
+    console.log(this.filtrado)
+  }
+
+} 

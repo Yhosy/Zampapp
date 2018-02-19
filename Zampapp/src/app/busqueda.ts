f1f813d16faea54e3bc122446170d16ed37ea83e
Angel <yhosy_soldier@hotmail.com>
2018-02-19 17:36:36 +0100

    Actualizacion

---
+++ Zampapp/src/app/busqueda.ts
@@ -0,0 +1,15 @@
+import{ Categoria} from './categoria'
+
+export class Busqueda {
+    constructor() {
+        
+    }
+
+    categoria: String;
+    cantidad: String;
+    preferencia: String;
+    temperatura: String;
+    picante: String;
+    precio:number;
+    proximidad: number;
+}

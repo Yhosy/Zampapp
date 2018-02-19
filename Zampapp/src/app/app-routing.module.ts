bc318aab1f2440cb9be160ed2a814c9eb778bf97
Angel <yhosy_soldier@hotmail.com>
2018-02-19 12:37:29 +0100

    Actualizacion

--- Zampapp/src/app/app-routing.module.ts
+++ Zampapp/src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {AppComponent} from './app.component'
 import {HomeComponent} from './home/home.component'
+// import {ResultadoComponent} from './resultado/resultado.component'
+// import {PreguntasComponent} from './preguntas/preguntas.component'
+
 
 
  
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
+  { path: 'Resultado', component: HomeComponent },
+  { path: 'Preguntas', component: HomeComponent },
+
+  
 ];
  
 @NgModule({

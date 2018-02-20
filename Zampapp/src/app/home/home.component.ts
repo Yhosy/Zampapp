80535441bb02d35f5282b6fc77e82d3e9ec24e36
Angel <yhosy_soldier@hotmail.com>
2018-02-20 11:30:23 +0100

    Home Actualizado

--- Zampapp/src/app/home/home.component.ts
+++ Zampapp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef } from '@angular/core';
 import { Categorias } from '../categorias';
 import { BusquedaService } from '../busqueda.service'
+import { QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { BusquedaService } from '../busqueda.service'
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  @ViewChildren('transparent') elements : QueryList<ElementRef>; 
   filtrado = [];
   valores = {};
   constructor(private busquedaService: BusquedaService) { }

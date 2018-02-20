2cb9f0dec82f1c6f9eee4d30b18ceb3faf2dde75
Angel <yhosy_soldier@hotmail.com>
2018-02-20 12:11:41 +0100

    Home Actualizado

--- Zampapp/src/app/home/home.component.ts
+++ Zampapp/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChildren, ElementRef } from '@angular/core';
 import { Categorias } from '../categorias';
 import { BusquedaService } from '../busqueda.service'
 import { QueryList } from '@angular/core';
+import {Busqueda} from '../busqueda'
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,8 @@ import { QueryList } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  @ViewChildren('transparent') elements : QueryList<ElementRef>; 
+  @ViewChildren('input') elements : QueryList<ElementRef>; 
   filtrado = [];
-  valores = {};
   constructor(private busquedaService: BusquedaService) { }
 
   ngOnInit() {
@@ -27,12 +27,18 @@ export class HomeComponent implements OnInit {
   }
 
   Hola(e) {
-    let inputs = document.getElementsByClassName('transparent')
+    // let patata=this.elements;
+    // console.log('patata',patata.)
+    this.busquedaService.Busqueda=new Busqueda;
+    
+    let inputs = document.getElementsByClassName('transparent');
     for (var i = 0; i < inputs.length; i++) {
-      this.busquedaService.Busqueda.categoria.push(JSON.stringify (inputs[i].attributes[4].value)) 
+      this.busquedaService.Busqueda.categoria.push(inputs[i].attributes[4].value); 
     }
-    console.log(this.busquedaService.Busqueda);
+    let patata=[];
+    for (var i = 0; i < inputs.length; i++) {
+      patata.push(inputs[i]); 
+    }
+    console.log(inputs)
   }
-
-
 } 

d6dde9962325de3e8c17ba51bc28d0b89c4cd9f9
Vita4a <vera.dyafu@gmail.com>
2018-02-20 23:03:48 +0100

    commit de los resultados

--- Zampapp/src/app/plato.service.ts
+++ Zampapp/src/app/plato.service.ts
@@ -10,12 +10,12 @@ export class PlatoService {
 	constructor(private http:Http){}
 
 	getPlatos(){
-		return this.http.get ( 'http://www.mocky.io/v2/5a82c5ad2f0000580074bb66')
+		return this.http.get ( 'http://www.mocky.io/v2/5a8c80463000007500323f54')
 			.map(response => response.json())			
 	}
 
 	getPlato(id: number) {
-		return this.http.get ( 'http://www.mocky.io/v2/5a82c5ad2f0000580074bb66')
+		return this.http.get ( 'http://www.mocky.io/v2/5a8c80463000007500323f54')
 			.map(response => response.json()
 			.filter(plato => plato.id ===id))	
 	}
--- Zampapp/src/app/resultados/resultados.component.ts
+++ Zampapp/src/app/resultados/resultados.component.ts
@@ -15,7 +15,6 @@ import { BusquedaService } from '../busqueda.service'
 export class ResultadosComponent implements OnInit {
 
     platos = [];
-    public valores = {categoria: [], cantidad: [], preferencia: [], temp: [], pic: [], precio:[], proximidad:[]}
 	listaFiltrada=[];
 
 	categorias = ['pizzas','hamburguesas', 'arroces', 'ensaladas', 'pastas', 'carnes', 'sopas', 'sushi']
@@ -24,9 +23,16 @@ export class ResultadosComponent implements OnInit {
 
 
 	onCheckboxChange(categoria, event) {
-		 let inputs = document.getElementsByClassName('transparent');
-	     this.busquedaService.Busqueda[event.target.name].push(event.target.value); 
-	     console.log(this.busquedaService.Busqueda);
+		 if(event.target.checked) {
+		  this.busquedaService.Busqueda[event.target.name].push(event.target.value);
+		} else {
+		  for(var i=0 ; i < this.categorias.length; i++) {
+		    if(this.busquedaService.Busqueda[event.target.name][i] == event.target.value){
+		      this.busquedaService.Busqueda[event.target.name].splice(i,1);
+		    }
+		  }
+		}
+		console.log(this.busquedaService.Busqueda);
 	}
 
 	constructor(private platoService: PlatoService, private busquedaService: BusquedaService){
@@ -34,14 +40,37 @@ export class ResultadosComponent implements OnInit {
 	
 
 	ngOnInit(){
+		
 		//this.users = this.usersService.users
 		this.platoService.getPlatos().subscribe(platos => {
 			console.log(platos);
 			this.platos = platos;
+			this.listaFiltrada=[];
+
+			for(let i=0; i<this.busquedaService.Busqueda.categoria.length; i++){
+				this.listaFiltrada = this.listaFiltrada.concat(this.platos.filter(plato => plato.categoria == this.busquedaService.Busqueda.categoria[i]));
+			}
+			for(let i=0; i<this.busquedaService.Busqueda.cantidad.length; i++){
+				this.listaFiltrada = this.listaFiltrada.concat(this.platos.filter(plato => plato.cantidad == this.busquedaService.Busqueda.cantidad[i]));
+			}
+			for(let i=0; i<this.busquedaService.Busqueda.temperatura.length; i++){
+				this.listaFiltrada = this.listaFiltrada.concat(this.platos.filter(plato => plato.temp == this.busquedaService.Busqueda.temperatura[i]));
+			}
+			for(let i=0; i<this.busquedaService.Busqueda.preferencia.length; i++){
+				this.listaFiltrada = this.listaFiltrada.concat(this.platos.filter(plato => plato.preferencia == this.busquedaService.Busqueda.preferencia[i]));
+			}
+			for(let i=0; i<this.busquedaService.Busqueda.picante.length; i++){
+				this.listaFiltrada = this.listaFiltrada.concat(this.platos.filter(plato => plato.pic == this.busquedaService.Busqueda.picante[i]));
+			}
+			this.listaFiltrada = this.listaFiltrada.reduce(function(acc, el, i, arr) {
+			  if (arr.indexOf(el) !== i && acc.indexOf(el) < 0) acc.push(el); return acc;
+			}, []);
+			console.log("lista filtrada", this.listaFiltrada);
 		});
 	}
 
 	onFilter(){
+		this.listaFiltrada=[];
 		if(this.busquedaService.Busqueda.categoria !== null){
 			for(let i=0; i<this.busquedaService.Busqueda.categoria.length; i++){
 				this.listaFiltrada = this.listaFiltrada.concat(this.platos.filter(plato => plato.categoria == this.busquedaService.Busqueda.categoria[i]));

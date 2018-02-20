4d06dde2ec83a63eb044db4fb4e25f9ea1b2a8a2
Vita4a <vera.dyafu@gmail.com>
2018-02-20 09:45:49 +0100

    App module commit

--- Zampapp/src/app/app.module.ts
+++ Zampapp/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
@@ -9,13 +8,10 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ResultadosComponent } from './resultados/resultados.component';
-<<<<<<< HEAD
 import { PlatosComponent } from './platos/platos.component';
 import  { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
-=======
 import { PreguntasComponent } from './preguntas/preguntas.component';
->>>>>>> 7235fe84464f859e1ca66c33d747e8407f006786
 
 import {PlatoDetailComponent} from './plato-detail/plato-detail.component';
 
@@ -26,12 +22,9 @@ import {PlatoDetailComponent} from './plato-detail/plato-detail.component';
     FooterComponent,
     HomeComponent,
     ResultadosComponent,
-<<<<<<< HEAD
-    PlatosComponent
-=======
+    PlatosComponent,
     PreguntasComponent,
     PlatoDetailComponent
->>>>>>> 7235fe84464f859e1ca66c33d747e8407f006786
   ],
   imports: [
     BrowserModule,

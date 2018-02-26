95bb2e393f71af30a975582448d25b1abf44eca4
marcosalbares <marcosalbares@gmail.com>
2018-02-26 10:53:40 +0100

    node service

--- Zampapp/src/app/app.module.ts
+++ Zampapp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { NodeService } from "./node.service";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,16 +9,15 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ResultadosComponent } from './resultados/resultados.component';
-import {BusquedaService} from './busqueda.service';
+import { BusquedaService } from './busqueda.service';
 import { PlatosComponent } from './platos/platos.component';
 import { PreguntasComponent } from './preguntas/preguntas.component';
-import {PlatoDetailComponent} from './plato-detail/plato-detail.component';
+import { PlatoDetailComponent } from './plato-detail/plato-detail.component';
 import { HttpModule } from '@angular/http';
 import { HoverDirective } from './hover.directive';
 import { HomeIzquierdaComponent } from './home-izquierda/home-izquierda.component';
 import { HomeDerechaComponent } from './home-derecha/home-derecha.component';
-
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -37,10 +36,14 @@ import { HomeDerechaComponent } from './home-derecha/home-derecha.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
     HttpModule
   ],
-  providers: [BusquedaService],
+  providers: [
+    BusquedaService,
+    NodeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

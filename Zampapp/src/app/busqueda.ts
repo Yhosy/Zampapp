4dca791e0e16e037f7e89652d1b7883da2b7300b
Vita4a <vera.dyafu@gmail.com>
2018-03-01 17:08:21 +0100

    Update busqueda.ts

--- Zampapp/src/app/busqueda.ts
+++ Zampapp/src/app/busqueda.ts
@@ -6,19 +6,17 @@ export class Busqueda {
         this.categoria=[];
         this.cantidad=[];
         this.preferencia=[];
-        this.temperatura=[];
-        this.picante=[];
-        this.precio=0;
-        this.proximidad=0;
+        this.temp=[];
+        this.pic=[];
+     
 
     }
 
     categoria: string[];
     cantidad: string[];
     preferencia: string[];
-    temperatura: string[];
-    picante: string[];
-    precio:number;
-    proximidad: number;
+    temp: string[];
+    pic: string[];
+   
 
 }
